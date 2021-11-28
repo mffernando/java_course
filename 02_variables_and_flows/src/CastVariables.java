0fe09425f214ad2b05652b7baf6bc18b08255680
Fernando <fmti91@hotmail.com>
2021-11-28 09:17:08 -0300

    cast variables

---
+++ 02_variables_and_flows/src/CastVariables.java
@@ -0,0 +1,17 @@
+
+public class CastVariables {
+
+	public static void main(String args[]) {
+		
+		double wage = 12500.12;
+		
+		System.out.println("My wage (double variable) " + wage);
+		
+		//cast double to int
+		int intWage = (int) wage;
+		
+		System.out.println("My wage (int variable) " + intWage);
+		
+	}
+	
+}
