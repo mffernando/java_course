d846d792355dde37905a6342e44435c6f11d3f7f
Fernando <fmti91@hotmail.com>
2021-11-28 09:04:09 -0300

    double variable

---
+++ 02_variables_and_flows/src/DoubleVariable.java
@@ -0,0 +1,16 @@
+
+public class DoubleVariable {
+	
+	public static void main(String args[]) {
+		
+		double wage = 1250.70;
+		
+		System.out.println("My wage is " + wage);
+		
+		double division = 3.14 / 2;
+		
+		System.out.println(division);
+		
+	}
+
+}
