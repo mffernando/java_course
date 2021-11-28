420fa29370a8eebbe91098064e3148d0dcdf2ceb
Fernando <fmti91@hotmail.com>
2021-11-28 08:26:31 -0300

    int variable

---
+++ 02_variables_and_flows/src/IntVariable.java
@@ -0,0 +1,33 @@
+
+public class IntVariable {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello!!!");
+		
+		//integer variable
+		int age;
+		age = 37;
+		
+		System.out.println(age);
+		
+		age = age + age;
+		
+		System.out.println(age);
+		
+		age = 10 * 2 + 5;
+		
+		System.out.println(age);
+		
+		age = 10 - 5 + 25;
+		
+		//concatenate string + int
+		System.out.println("Your age is " + age);
+		
+		age = 2 * 10 / 2 + 10;
+		
+		System.out.println("You are " + age + " years old!");
+		
+	}
+
+}
