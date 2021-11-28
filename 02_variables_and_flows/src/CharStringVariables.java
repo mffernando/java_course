8230e7c4d82d86a66e96fa8aba5810228b48434d
Fernando <fmti91@hotmail.com>
2021-11-28 09:34:51 -0300

    char and String variables

---
+++ 02_variables_and_flows/src/CharStringVariables.java
@@ -0,0 +1,24 @@
+
+public class CharStringVariables {
+
+	public static void main(String args[]) {
+		
+		//char
+		char myChar = 'a';
+		System.out.println("My Char: " + myChar);
+		
+		char myNewChar = 66;
+		System.out.println("My New Char: " + myNewChar);
+		
+		char myNewCastChar = (char) (myNewChar + 1);
+		System.out.println("My New Cast Char: " + myNewCastChar);
+		
+		//String
+		String myText = "My new text";
+		System.out.println(myText);
+		
+		//concat
+		System.out.println(myText + " in "  + 2021);
+	}
+	
+}
