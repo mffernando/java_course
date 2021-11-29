a853894bad640e20782aa8451f8efb9c87b006ba
Fernando <fmti91@hotmail.com>
2021-11-29 20:28:13 -0300

    test while

---
+++ 02_variables_and_flows/src/TestWhile.java
@@ -0,0 +1,17 @@
+
+public class TestWhile {
+
+	public static void main(String[] args) {
+		
+		int count = 0;
+		
+		while (count <= 10) {
+			System.out.println("Count = " + count);
+			//count = count + 1;
+			//count += 1;
+			count ++;
+		}
+		
+	}
+	
+}
