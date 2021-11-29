529d527b7aca35ac14f7be014fb6389a02e2ece6
Fernando <fmti91@hotmail.com>
2021-11-29 20:38:31 -0300

    test for

---
+++ 02_variables_and_flows/src/TestFor.java
@@ -0,0 +1,12 @@
+
+public class TestFor {
+
+	public static void main(String[] args) {
+		
+		for (int count = 0; count <= 10; count++) {
+			System.out.println("Count = " + count);
+		}
+		
+	}
+	
+}
