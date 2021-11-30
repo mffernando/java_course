78b20749a62c0d512e0b029dfb26b66e52daaa86
Fernando <fmti91@hotmail.com>
2021-11-29 21:10:25 -0300

    multiples of three

---
+++ 02_variables_and_flows/src/MultiplesOfThree.java
@@ -0,0 +1,17 @@
+
+public class MultiplesOfThree {
+
+	public static void main(String[] args) {
+		
+		for (int number = 1; number <= 100; number++) {
+			
+			if (number % 3 == 0) {
+				
+				System.out.println("The number " + number + " is a multiple of three!!");
+			}
+			
+		}
+		
+	}
+
+}
