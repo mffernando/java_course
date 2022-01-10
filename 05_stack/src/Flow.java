cfe0213c39b4b2684334ed2d0ff050ca1b759ce8
Fernando <fmti91@hotmail.com>
2022-01-10 17:57:04 -0300

    try catch exceptions

--- 05_stack/src/Flow.java
+++ 05_stack/src/Flow.java
@@ -1,23 +1,30 @@
 
 public class Flow {
 
-    public static void main(String[] args) {
-        System.out.println("start main");
-        method1();
-        System.out.println("end main");
-    }
+	public static void main(String[] args) {
+		System.out.println("start main");
+		try {
+			method1();
+		} catch (ArithmeticException | NullPointerException e) {
+			System.out.println("ArithmeticException: " + e);
+		}
+		System.out.println("end main");
+	}
 
-    private static void method1() {
-        System.out.println("start method 1");
-        method2();
-        System.out.println("end method 1");
-    }
+	private static void method1() {
+		System.out.println("start method 1");
+		method2();		
+		System.out.println("end method 1");
+	}
 
-    private static void method2() {
-        System.out.println("start method 2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("end method 2");
-    }
+	private static void method2() {
+		System.out.println("start method 2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0; // force ArithmeticException
+			Test t = null; // NullPointerException
+			t.func();
+		}
+		System.out.println("end method 2");
+	}
 }
