d7e36af24299464d68b268abba4a710c605a09ed
Fernando <fmti91@hotmail.com>
2022-02-07 19:19:51 -0300

    Array

---
+++ 07_array/src/Array.java
@@ -0,0 +1,17 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numbers = new int[10];
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i * i;
+			System.out.println(numbers[i]);
+			
+		}
+		
+	}
+
+}
